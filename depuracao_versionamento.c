d23bdf6d76882e0ff7bc879dd01a83550d4803fd
wedsonjswd <wedson1984@gmail.com>
2024-12-18 17:06:14 -0300

    Arquivo depuracao_versionamento.c  modificado.

--- depuracao_versionamento.c
+++ depuracao_versionamento.c
@@ -4,7 +4,7 @@ int calc_fat(int);
 
 int main()
 {
-    int numero = 5;
+    int numero = 10; //modificando essa linha
     int num_fat = calc_fat(numero);
     printf("\nO fatorial de %d eh: %d", numero, num_fat);
     return 0;
